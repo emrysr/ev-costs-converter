e0b95e9be09d8dc5e5f4580f78070ca3be049999
Emrys Roberts <emrys.r@gmail.com>
2026-08-22 22:18:46 +0100

    Add Octopus Agile regions constant with no default region

--- src/constants/defaults.ts
+++ src/constants/defaults.ts
@@ -6,3 +6,26 @@ export const DEFAULT_SHAKE_ENABLED = true
 export const DEFAULT_BATTERY_CAPACITY = 60
 export const DEFAULT_CURRENT_SOC = 20
 export const DEFAULT_TARGET_SOC = 80
+export const DEFAULT_AGILE_REGION = null
+
+// Octopus Agile tariff regions (DNO regions)
+export const AGILE_REGIONS = {
+  A: 'Eastern England (UKPN - East Anglia)',
+  B: 'East Midlands (UKPN - East Midlands)',
+  C: 'London (UKPN - South Eastern)',
+  D: 'North Wales, Merseyside & Cheshire (SP Manweb)',
+  E: 'West Midlands (UKPN - West Midlands)',
+  F: 'Northern Scotland (SEEL - North)',
+  G: 'Central Scotland (SEEL - South)',
+  H: 'Southern Scotland (SEEL - South West)',
+  J: 'Northern Electric (Northern Powergrid - Northern)',
+  K: 'Yorkshire (Northern Powergrid - Yorkshire)',
+  L: 'North Eastern England (Northern Powergrid - North Eastern)',
+  M: 'South Wales (South Wales Electricity)',
+  N: 'South East England (SEEL - South Eastern)',
+  O: 'North West (Electricity North West)',
+  P: 'South West England (South West Water)',
+  R: 'Southern Electric Region (Electricity South West)'
+} as const
+
+export type AgileRegionCode = keyof typeof AGILE_REGIONS
